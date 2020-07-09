c8d8cf82a40e2019ccedbf18d8044e11ce58e304
Giovanno Rachmat <giovannorachmat@gmail.com>
2020-07-09 15:11:31 +0700

    update table.sql

--- datasets/tables.sql
+++ datasets/tables.sql
@@ -1,33 +1,37 @@
-create table crm_events (
-  date_received date
-  ,product varchar
-  ,sub_product varchar
-  ,issue varchar
-  ,sub_issue varchar
-  ,consumer_complaint_narrative varchar
-  ,tags varchar
-  ,consumer_consent_provided varchar
-  ,submitted_via varchar
-  ,date_sent_to_company date
-  ,company_response_to_consumer varchar
-  ,timely_response boolean
-  ,consumer_disputed boolean
-  ,complaint_id varchar
-  ,client_id varchar
+CREATE TABLE crm_events (
+  date_received DATE
+  ,product VARCHAR
+  ,sub_product VARCHAR
+  ,issue VARCHAR
+  ,sub_issue VARCHAR
+  ,consumer_complaint_narrative VARCHAR
+  ,tags VARCHAR
+  ,consumer_consent_provided VARCHAR
+  ,submitted_via VARCHAR
+  ,date_sent_to_company DATE
+  ,company_response_to_consumer VARCHAR
+  ,timely_response BOOLEAN
+  ,consumer_disputed BOOLEAN
+  ,complaint_id VARCHAR
+  ,client_id VARCHAR
 );
 
-create table crm_call_center_logs (
-  date_received date
-  ,complaint_id varchar
-  ,rand_client varchar
-  ,phone_final varchar
-  ,vru_line varchar
-  ,call_id int
-  ,priority varchar
-  ,type varchar
-  ,outcome varchar
-  ,server varchar
-  ,ser_start time
-  ,ser_exit time
-  ,ser_time time
+COPY crm_call_center_logs FROM '/Users/giovannorachmat/Git (Personal)/dkatalis-take-home-test-bi-analyst/datasets/crm_call_center_logs.csv' CSV HEADER;
+
+CREATE TABLE crm_call_center_logs (
+  date_received DATE
+  ,complaint_id VARCHAR
+  ,rand_client VARCHAR
+  ,phone_final VARCHAR
+  ,vru_line VARCHAR
+  ,call_id INT
+  ,priority VARCHAR
+  ,type VARCHAR
+  ,outcome VARCHAR
+  ,server VARCHAR
+  ,ser_start TIME
+  ,ser_exit TIME
+  ,ser_time TIME
 );
+
+COPY crm_events FROM '/Users/giovannorachmat/Git (Personal)/dkatalis-take-home-test-bi-analyst/datasets/crm_events.csv' CSV HEADER;
